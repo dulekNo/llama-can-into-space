c9ef454a8e0eb7ce028dc3da94dab4d986864896
Igor Siejkowski <dulcekun@gmail.com>
2019-03-01 14:38:44 +0100

    second part of tutorial done

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,13 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
-	</div>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  	name: "About"
+  name: "About"
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
